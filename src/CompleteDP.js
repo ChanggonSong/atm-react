87e7958c79e7be558e7b7acd55f96e674e0ca3d5
Ruila <113187890+RustTale@users.noreply.github.com>
2024-06-12 21:43:53 +0900

    Update CompleteDP.js

--- src/CompleteDP.js
+++ src/CompleteDP.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Complete.css';
 import logo from "./img/logo.png";
 
 
 function CompleteDP({ balance }) {
 
+    const navigate = useNavigate();
+
+    const backHomeClick = () => {
+        navigate('/main');
+      };
+    
 
     return (
       <div className="background">
@@ -14,6 +21,7 @@ function CompleteDP({ balance }) {
               <h2>입금 완료되었습니다</h2>
               <h3>이용해주셔서 감사합니다</h3>
               <h3>잔고 : {balance}</h3>
+              <button className="backhome_button" onClick={backHomeClick}>홈으로 돌아가기</button>
           </div>
       </div>
     );
