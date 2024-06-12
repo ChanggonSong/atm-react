23caf47a52ef1f89ad4dff180f5f6c8caff9f602
Changgon Song <gon05158557@gmail.com>
2024-06-12 21:24:53 +0900

    Add env file

--- src/App.js
+++ src/App.js
@@ -10,24 +10,23 @@ import CompleteTF from './CompleteTF';
 import InquiryTrans from './InquiryTrans';
 
 
-
 function App() {
   return (
-      <div>
+    <div>
         <Router basename={process.env.PUBLIC_URL}> 
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="/login" element={<FirstPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/enterDP" element={<EnterDeposit />} />
-        <Route path="/enterWD" element={<EnterWithdraw />} />
-        <Route path="/completeDP" element={<CompleteDP />} />
-        <Route path="/completeWD" element={<CompleteWD />} />
-        <Route path="/completeTF" element={<CompleteTF />} />
-        <Route path="/inquiry" element={<InquiryTrans />} />
-        {/* Default route to handle direct access to the root */}
-      </Routes>
-    </Router>
+          <Routes>
+            <Route path="/" element={<FirstPage />} />
+            <Route path="/login" element={<FirstPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/enterDP" element={<EnterDeposit />} />
+            <Route path="/enterWD" element={<EnterWithdraw />} />
+            <Route path="/completeDP" element={<CompleteDP />} />
+            <Route path="/completeWD" element={<CompleteWD />} />
+            <Route path="/completeTF" element={<CompleteTF />} />
+            <Route path="/inquiry" element={<InquiryTrans />} />
+            {/* Default route to handle direct access to the root */}
+          </Routes>
+        </Router>
       </div>
   );
 }
